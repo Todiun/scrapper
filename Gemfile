7136445ac1cb20b9034e61f6e59029d94f057b57
Todiun <joquch@hotmail.co.uk>
2025-09-18 17:12:58 +0200

    Finished crypto exercise

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 ruby '3.4.2'
 gem 'rspec'
-gem 'pry'
-gem 'rubocop'
 gem 'nokogiri'
-gem 'open-uri'
+gem 'net'
+gem 'json'
+gem 'dotenv'
--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,14 +1,44 @@
+require 'net/http'
+require 'json'
 require 'nokogiri'
-require 'open-uri'
+require 'dotenv/load'
 
+API_KEY = ENV["API_KEY"] 
 
-url= "https://coinmarketcap.com/all/views/all/"
-page = Nokogiri::HTML(URI.open(url))
+url = URI("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=20&convert=USD")
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
 
-puts page.class
+request = Net::HTTP::Get.new(url)
+request["X-CMC_PRO_API_KEY"] = API_KEY
+request["Accept"] = "application/json"
 
-page.css('tr').first(20).each do |row|
-  name  = row.css('td.cmc-table__cell--sort-by__name a').text.strip
-  price = row.css('td.cmc-table__cell--sort-by__price a').text.strip
-  puts "#{name} - #{price}" unless name.empty?
+response = http.request(request)
+
+if response.code == "200"
+  data = JSON.parse(response.body)
+
+  
+  html_string = "<coins>"
+  data["data"].each do |coin|
+    html_string += "<coin>"
+    html_string += "<symbol>#{coin['symbol']}</symbol>"
+    html_string += "<name>#{coin['name']}</name>"
+    html_string += "<price>#{coin['quote']['USD']['price'].round(2)}</price>"
+    html_string += "</coin>"
+  end
+  html_string += "</coins>"
+
+  
+  doc = Nokogiri::XML(html_string)
+
+  
+  doc.xpath("//coin").each do |coin_node|
+    symbol = coin_node.at_xpath("symbol").text
+    name   = coin_node.at_xpath("name").text
+    price  = coin_node.at_xpath("price").text
+    puts "#{symbol} (#{name}) - $#{price}"
+  end
+else
+  puts "Error: #{response.code} #{response.message}"
 end
